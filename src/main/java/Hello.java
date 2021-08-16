90cf4ebb1c40eb3376a8d16899f8947b7f16d749
AinijiangMMT <ainijiang@outlook.com>
2021-08-16 15:33:59 +0200

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("hello feature ");
+        System.out.println("local repo is already published to GitHub repo");
+        System.out.println("doing more work");
+
+
     }
 }
